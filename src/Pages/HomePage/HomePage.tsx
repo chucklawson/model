15a47b47ff4d735eeda4cd03ad46d3de4eabb802
CharlesLawson <charles.thomas.lawson@gmail.com>
2025-10-24 10:48:32 -0400

    centered images

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -20,40 +20,38 @@ return(
     <>
       <div className="text-center">
         <header className="bg-green-100 text-green-600 text-3xl font-bold h-20 justify-items-center p-5">
-            <div>
-                {headerValue}
-            </div>
-        </header>
-
-        <>
           <div>
-            <a href="https://vite.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+            {headerValue}
           </div>
-          <h1 className='text-red-500'>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-        </>
-
+        </header>
       </div>
 
+      <>
+        <div className="images">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo"/>
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo"/>
+          </a>
+        </div>
 
-
-
+        <h1 className='text-red-500'>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
       </>
-  )
+
+
+    </>
+)
 }
 export default HomePage;
